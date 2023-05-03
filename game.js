d614669ea619403fa0d89324e96e744b32f65d9e
Rafał Konieczny <rkonieczny87@gmail.com>
2023-05-03 20:15:28 +0200

    code cleanup

--- game.js
+++ game.js
@@ -1,6 +1,5 @@
 const gameTiles = document.querySelectorAll(".tile");
 const gameBoard = document.querySelector("#game-board");
-
 const gameState = [
   [gameTiles[0], gameTiles[1], gameTiles[2]],
   [gameTiles[3], gameTiles[4], gameTiles[5]],
@@ -10,25 +9,22 @@ const gameState = [
 function render(gameBoard, gameState) {
   gameState.forEach((row, rowIndex) => {
     row.forEach((column, columnIndex) => {
-        column.style.top = `${rowIndex * 100}px`;
-        column.style.left = `${columnIndex * 100}px`;
-
-        column.style['background-position-y'] = `-${rowIndex*100}px`;
-        column.style['background-position-x'] = `-${columnIndex*100}px`;
+      column.style.top = `${rowIndex * 100}px`;
+      column.style.left = `${columnIndex * 100}px`;
+      column.style["background-position-y"] = `-${rowIndex * 100}px`;
+      column.style["background-position-x"] = `-${columnIndex * 100}px`;
       gameBoard.appendChild(column);
     });
   });
 }
 
 function moveElement(element1, element2) {
-    const tempTop = element1.style.top;
-    const tempLeft = element1.style.left;
-
-    element1.style.top = element2.style.top;
-    element1.style.left = element2.style.left;
-
-    element2.style.top = tempTop;
-    element2.style.left = tempLeft;
+  const tempTop = element1.style.top;
+  const tempLeft = element1.style.left;
+  element1.style.top = element2.style.top;
+  element1.style.left = element2.style.left;
+  element2.style.top = tempTop;
+  element2.style.left = tempLeft;
 }
 
 render(gameBoard, gameState);
@@ -37,7 +33,6 @@ gameBoard.addEventListener("click", (event) => {
   const target = event.target;
 
   let x, y;
-
   gameState.forEach((row, rowIndex) => {
     row.forEach((column, columnIndex) => {
       if (column === target) {
@@ -48,7 +43,6 @@ gameBoard.addEventListener("click", (event) => {
   });
 
   let emptyX, emptyY;
-
   gameState.forEach((row, rowIndex) => {
     row.forEach((column, columnIndex) => {
       if (column.innerText === "") {
@@ -62,9 +56,7 @@ gameBoard.addEventListener("click", (event) => {
     (y === emptyY && (x + 1 === emptyX || x - 1 === emptyX)) ||
     (x === emptyX && (y + 1 === emptyY || y - 1 === emptyY))
   ) {
-
     moveElement(gameState[x][y], gameState[emptyX][emptyY]);
-
     const temp = gameState[x][y];
     gameState[x][y] = gameState[emptyX][emptyY];
     gameState[emptyX][emptyY] = temp;
